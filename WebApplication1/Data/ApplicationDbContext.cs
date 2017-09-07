123e26e3bc86c339ff026c082af3fc6f52b19947
Troother <Troother@DESKTOP-BRBQ07J>
2017-09-07 10:34:01 +0200

    Lägg till ett DbSet av Product med namnet Products till ApplicationDbContext.   Gör en commit.

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -22,5 +22,6 @@ namespace WebApplication1.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+        public DbSet<Product> Products { get; set; }
     }
 }
