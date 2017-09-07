f557cd60c59e016ad5a190f13da8f30932f7004e
Troother <Troother@DESKTOP-BRBQ07J>
2017-09-07 11:34:49 +0200

    - Prepopulera ProductCategories med tre kategorier: "TV", "DVD" och "VHS".
    - Lägg till ett menyval i _Layout för "Products". Index för controllern ska visas när valet väljs.

---
+++ WebApplication1/Data/DbSeed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DbSeed
+    {
+        public static void Seed(ApplicationDbContext context)
+        { 
+        var tv = new ProductCategory() { ProductCategoryId = 1, Name = "TV" };
+        var dvd = new ProductCategory() { ProductCategoryId = 2, Name = "DVD" };
+        var vhs = new ProductCategory() { ProductCategoryId = 3, Name = "VHS" };
+
+        context.SaveChanges();
+        }
+    }
+}
