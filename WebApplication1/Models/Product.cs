4a7f206ec75c6504b6104dd6238f2748861b5f08
Troother <Troother@DESKTOP-BRBQ07J>
2017-09-07 10:37:07 +0200

    Lägg till valideringsattribut på Name (Required) och Price (Range mellan 5 och 1000).

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace WebApplication1.Models
     public class Product
     {
         public int ProductId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Range(5, 1000)]
         public decimal Price { get; set; }
     }
 }
